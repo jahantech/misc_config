7ab4db642b7395a6321861e8f2b4613a336506cd
jahantech <jahansyed@hotmail.co.uk>
2017-10-28 22:08:30 +0100

    Updated url

--- kafka-zookeeper-install.sh
+++ kafka-zookeeper-install.sh
@@ -3,6 +3,6 @@
 apt update
 apt install default-jre
 apt install zookeeperd
-wget "https://mirror.cc.columbia.edu/pub/software/apache/kafka/0.8.2.1/kafka_2.11-0.8.2.1.tgz"
+wget "http://mirror.cc.columbia.edu/pub/software/apache/kafka/0.8.2.1/kafka_2.11-0.8.2.1.tgz"
 tar -xvf kafka_*
 nohup ~/kafka/bin/kafka-server-start.sh ~/kafka/config/server.properties > ~/kafka/kafka.log 2>&1 & 
